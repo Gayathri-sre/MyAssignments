42aa36f76b4634135221fa4995dde7aa9ae0a258
Gayathri-sre <gayathrisreaadh@gmail.com>
2025-10-15 16:54:26 +0530

    Add files via upload
    Day1 - Datatypes basics

---
+++ Day1/console.js
@@ -0,0 +1,15 @@
+//console is similar to print
+console.log ("Welcome to Playwrite")
+
+//To execute - node <filename.js>
+var firstName = 'Gayathri'
+var companyName = 'TestLeaf'
+var mobileNumber = 123456789
+var isautomation = true 
+var hasPlaywright
+
+console.log("Datatype : " +typeof (firstName)+" --- My name is "+firstName)
+console.log("Datatype : " +typeof (companyName)+" --- I learn from :"+companyName)
+console.log("Datatype : " +typeof (mobileNumber)+" --- Contact me from :"+mobileNumber)
+console.log("Datatype : " +typeof (isautomation)+" --- Is Playwright an automation tool ? "+isautomation)
+console.log("Datatype : " +typeof (hasPlaywright))
