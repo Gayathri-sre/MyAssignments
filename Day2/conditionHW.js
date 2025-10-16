2fbb86a9c60d46b0e710c0a4ac74043da19d20b8
Gayathri-sre <gayathrisreaadh@gmail.com>
2025-10-16 12:40:09 +0530

    Add files via upload
    If condition home task

---
+++ Day2/conditionHW.js
@@ -0,0 +1,13 @@
+//Condition Assignment
+
+let mark=80  //Using let operator
+//if condition
+if (mark>80) {
+    console.log("Grade A : "+mark)
+} else if (mark>75) {
+    console.log("Grade B : "+mark)
+} else if (mark>55) {
+    console.log("Grade C : "+mark)
+} else {
+    console.log("Grade D : "+mark)
+}
