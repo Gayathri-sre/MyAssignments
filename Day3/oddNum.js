54d5e371c4a65174d155ff5844691edaea606fa7
Gayathri-sre <gayathrisreaadh@gmail.com>
2025-10-21 15:00:17 +0530

    Add files via upload
    Switch and case statements

---
+++ Day3/oddNum.js
@@ -0,0 +1,13 @@
+//Print Odd Numbers 
+let number=30
+
+//for loop
+//if condition
+
+for (let i=0; i<=number;i++) {
+//console.log(i)
+    if(i%2==1) {
+        console.log("Odd number :"+i)
+    }
+
+}
