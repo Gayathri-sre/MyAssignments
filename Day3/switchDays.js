3fbccfcdc73634f65ded6992ef9ca7bb76f1d2b8
Gayathri-sre <gayathrisreaadh@gmail.com>
2025-10-21 16:34:05 +0530

    Add files via upload
    Example for switch

--- Day3/switchDays.js
+++ Day3/switchDays.js
@@ -1,34 +1,34 @@
 //Switch Case with Days
 
-let days ="4"
+let days ="5"
 
 switch(days) {
 
-    case "0":
+    case "1":
         console.log("It is Sunday")
     break;
 
-    case "1":
+    case "2":
         console.log("It is Monday")
     break;
 
-    case "2":
+    case "3":
         console.log("It is Tuesday")
     break;
     
-    case "3":
+    case "4":
         console.log("It is Wednesday")
     break;
     
-    case "4":
+    case "5":
         console.log("It is Thursday")
     break;
         
-    case "5":
+    case "6":
         console.log("It is Friday")
     break;
 
-    case "6":
+    case "7":
         console.log("It is Saturday")
     break;
 
